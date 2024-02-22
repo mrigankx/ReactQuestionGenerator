30ced0e64c16b6eedb66d93d38bc0515d207e0d7
mriganka <mrigankagoswami03@gmail.com>
2024-02-22 21:33:50 +0530

    added questions and timer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { reactQues } from "./questions";
 function App() {
+  const [question, setQuestion] = useState("What is React?");
+  const [start, setStart] = useState(false);
+  const [secs, setSecs] = useState(60);
+
+  useEffect(() => {
+    let id = null;
+    if (start && secs > 0) {
+      id = setInterval(() => {
+        setSecs((prev) => prev - 1);
+      }, 1000);
+    } else {
+      clearInterval(id);
+    }
+    return () => clearInterval(id);
+  }, [start, question, secs]);
+  const setNewQuestion = (prev) => {
+    let qId = Math.floor(Math.random() * reactQues.length);
+    return reactQues[qId];
+  };
+  useEffect(() => {
+    var id = null;
+    if (start && secs > 0) {
+      id = setInterval(() => {
+        setQuestion((prev) => {
+          return setNewQuestion(prev);
+        });
+      }, secs * 1000);
+    } else {
+      clearInterval(id);
+    }
+    return () => {
+      clearInterval(id);
+    };
+  }, [start, question, secs]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h4>{secs}</h4>
+        <h2>{question}</h2>
+        <button onClick={() => setStart(!start)}>
+          {start ? "Stop" : "Start"}
+        </button>
+        <button
+          onClick={() => {
+            setQuestion((prev) => {
+              return setNewQuestion(prev);
+            });
+            setSecs(60);
+          }}
         >
-          Learn React
-        </a>
+          NEXT
+        </button>
       </header>
     </div>
   );
