f3fef8fca9c43e4c9a71db94c0b9a49386297c8a
mriganka <mrigankagoswami03@gmail.com>
2024-02-27 16:57:54 +0530

    optimized

--- src/App.js
+++ src/App.js
@@ -6,24 +6,17 @@ function App() {
   const [start, setStart] = useState(false);
   const [secs, setSecs] = useState(60);
 
-  useEffect(() => {
-    let id = null;
-    if (start && secs > 0) {
-      id = setInterval(() => {
-        setSecs((prev) => prev - 1);
-      }, 1000);
-    } else {
-      clearInterval(id);
-    }
-    return () => clearInterval(id);
-  }, [start, question, secs]);
   const setNewQuestion = (prev) => {
     let qId = Math.floor(Math.random() * reactQues.length);
     return reactQues[qId];
   };
   useEffect(() => {
     var id = null;
+    let timerId = null;
     if (start && secs > 0) {
+      timerId = setInterval(() => {
+        setSecs((prev) => prev - 1);
+      }, 1000);
       id = setInterval(() => {
         setQuestion((prev) => {
           return setNewQuestion(prev);
@@ -33,6 +26,7 @@ function App() {
       clearInterval(id);
     }
     return () => {
+      clearInterval(timerId);
       clearInterval(id);
     };
   }, [start, question, secs]);
@@ -42,19 +36,25 @@ function App() {
       <header className="App-header">
         <h4>{secs}</h4>
         <h2>{question}</h2>
-        <button onClick={() => setStart(!start)}>
-          {start ? "Stop" : "Start"}
-        </button>
-        <button
-          onClick={() => {
-            setQuestion((prev) => {
-              return setNewQuestion(prev);
-            });
-            setSecs(60);
+        <div
+          style={{
+            display: "flex",
           }}
         >
-          NEXT
-        </button>
+          <button onClick={() => setStart(!start)}>
+            {start ? "Stop" : "Start"}
+          </button>
+          <button
+            onClick={() => {
+              setQuestion((prev) => {
+                return setNewQuestion(prev);
+              });
+              setSecs(60);
+            }}
+          >
+            NEXT
+          </button>
+        </div>
       </header>
     </div>
   );
